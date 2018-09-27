d8f668d87a60e2c96417a1e228b81087e55d38cb
Eric Backhage <eric.backhage@gmail.com>
2018-09-27 22:30:48 +0200

    Implementation of min heap priority queue

---
+++ PriorityQueue.Test/PriorityQueueTests.cs
@@ -0,0 +1,98 @@
+using System;
+using Xunit;
+
+namespace PriorityQueue.Test
+{
+    public class PriorityQueueTests
+    {
+        private readonly PriorityQueue<ComparableThing> _pq = new PriorityQueue<ComparableThing>();
+
+        [Fact]
+        public void AnItemThatIsEnqueued_CanBeDequeued()
+        {
+            var t = new ComparableThing(1);
+            _pq.Enqueue(t);
+
+            var r = _pq.Dequeue();
+
+            Assert.Equal(t, r);
+        }
+
+        [Fact]
+        public void HighestPriorityItems_GetsDequedFirst()
+        {
+            var lowPrio = new ComparableThing(3);
+            var mediumPrio = new ComparableThing(2);
+            var highPrio = new ComparableThing(1);
+
+            _pq.Enqueue(lowPrio);
+            _pq.Enqueue(highPrio);
+            _pq.Enqueue(mediumPrio);
+
+            Assert.Equal(highPrio, _pq.Dequeue());
+            Assert.Equal(mediumPrio, _pq.Dequeue());
+            Assert.Equal(lowPrio, _pq.Dequeue());
+        }
+
+        [Fact]
+        public void ItemsWithSamePriority_GetsDequedInTheOrderTheyWereAdded()
+        {
+            var item1 = new ComparableThing(1);
+            var item2 = new ComparableThing(1);
+            var item3 = new ComparableThing(1);
+
+            _pq.Enqueue(item1);
+            _pq.Enqueue(item2);
+            _pq.Enqueue(item3);
+
+            Assert.Equal(item1, _pq.Dequeue());
+            Assert.Equal(item2, _pq.Dequeue());
+            Assert.Equal(item3, _pq.Dequeue());
+        }
+
+        [Fact]
+        public void QueueCanHandleLargeAmountsOfItems_WithGoodPerformance()
+        {
+            const int itemCount = 100_000;
+            var rnd = new Random();
+
+            for (var i = 0; i < itemCount; i++)
+            {
+                var item = new ComparableThing(rnd.Next());
+                _pq.Enqueue(item);
+            }
+
+            var lastItem = _pq.Dequeue();
+
+            for (var i = 1; i < itemCount; i++)
+            {
+                var item = _pq.Dequeue();
+                Assert.True(item.CompareTo(lastItem) >= 0);
+                lastItem = item;
+            }
+        }
+    }
+
+    public class ComparableThing : IComparable<ComparableThing>
+    {
+        private readonly int _priority;
+
+        public ComparableThing(int priority)
+        {
+            _priority = priority;
+        }
+
+        public int CompareTo(ComparableThing other)
+        {
+            if (_priority > other._priority)
+            {
+                return 1;
+            }
+            else if (_priority < other._priority)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        private readonly List<T> _items = new List<T>();
+
+        public void Enqueue(T item)
+        {
+            _items.Add(item);
+            BubbleUp();
+        }
+
+        private void BubbleUp()
+        {
+            var childIndex = _items.Count - 1;
+            while (childIndex > 0)
+            {
+                var parentIndex = (childIndex - 1) / 2;
+                if (_items[childIndex].CompareTo(_items[parentIndex]) >= 0)
+                {
+                    break;
+                }
+                Swap(childIndex, parentIndex);
+                childIndex = parentIndex;
+            }
+        }
+
+        public T Dequeue()
+        {
+            var highestPrioritizedItem = _items[0];
+
+            MoveLastItemToTheTop();
+            SinkDown();
+
+            return highestPrioritizedItem;
+        }
+
+        private void MoveLastItemToTheTop()
+        {
+            var lastIndex = _items.Count - 1;
+            _items[0] = _items[lastIndex];
+            _items.RemoveAt(lastIndex);
+        }
+
+        private void SinkDown()
+        {
+            var lastIndex = _items.Count - 1;
+            var parentIndex = 0;
+
+            while (true)
+            {
+                var leftChildIndex = parentIndex * 2 + 1;
+                if (leftChildIndex > lastIndex)
+                {
+                    break;
+                }
+                var rightChildIndex = leftChildIndex + 1;
+                if (rightChildIndex <= lastIndex && _items[rightChildIndex].CompareTo(_items[leftChildIndex]) < 0)
+                {
+                    leftChildIndex = rightChildIndex;
+                }
+                if (_items[parentIndex].CompareTo(_items[leftChildIndex]) <= 0)
+                {
+                    break;
+                }
+                Swap(parentIndex, leftChildIndex);
+                parentIndex = leftChildIndex;
+            }
+
+        }
+
+        private void Swap(int index1, int index2)
+        {
+            var tmp = _items[index1];
+            _items[index1] = _items[index2];
+            _items[index2] = tmp;
+        }
+    }
+}
